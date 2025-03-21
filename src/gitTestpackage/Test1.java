9f44ad013355b3b3b5fced357b593493e94eb384
VKD-Code <vkd2799@gmail.com>
2025-03-21 15:53:15 +0530

    First commit...

---
+++ src/gitTestpackage/Test1.java
@@ -0,0 +1,27 @@
+package gitTestpackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+
+public class Test1 {
+
+	public static void main(String... args) {
+	
+	
+	WebDriver driver =new ChromeDriver();
+	    	SoftAssert softAssert=new SoftAssert();
+	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+
+	        driver.get("https://deployqa.deepfreeze.com/");
+	        String email = "vikashd@alohatechnologydev.com";
+	        String password = "Aloha@123";
+	        driver.findElement(By.name("EmailAddress")).sendKeys(email);
+	        driver.findElement(By.name("Password")).sendKeys(password);
+	        driver.findElement(By.xpath("//input[@value='Sign in']")).click();
+}
+}
